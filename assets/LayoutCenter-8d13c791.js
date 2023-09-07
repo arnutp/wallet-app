713992af3b877b1caaa1ed62d0bae132936a39d4
arnut.dev <arnut.dev@gmail.com>
2023-09-07 22:56:08 +0700

    rebuild

--- assets/LayoutCenter-8d13c791.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./_plugin-vue_export-helper-c27b6911.js";import{g as s,o as t,a as c,b as n}from"./index-41e4f233.js";const r={},a={class:"flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8"};function m(l,_){const e=s("RouterView");return t(),c("div",a,[n(e,{class:"m-4 sm:mx-auto sm:w-full sm:max-w-sm"})])}const p=o(r,[["render",m]]);export{p as default};
